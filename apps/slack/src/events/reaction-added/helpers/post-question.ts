be04c34d2c22cd894a14f0d8478ecad2eebdd391
Karibash <y.karibayashi@gmail.com>
2024-08-26 19:09:33 +0900

    feature: スキルをDBから取得して表示するようにする

--- apps/slack/src/events/reaction-added/helpers/post-question.ts
+++ apps/slack/src/events/reaction-added/helpers/post-question.ts
@@ -1,7 +1,20 @@
+import { database, schema } from '@feedbackun/package-database';
+import { asc, eq } from 'drizzle-orm';
 import { ResultAsync } from 'neverthrow';
 
 import type { SlackChannelId, SlackUserId } from '@feedbackun/package-domain';
 import type { SlackAPIClient } from 'slack-edge';
+
+type Option = {
+  id: string;
+  name: string;
+  level: number;
+  elements: Array<{
+    id: string;
+    order: number;
+    name: string;
+  }>;
+};
 
 export const postQuestion = ResultAsync.fromThrowable(async (
   client: SlackAPIClient,
@@ -13,6 +26,38 @@ export const postQuestion = ResultAsync.fromThrowable(async (
     channel: channelId.value,
     message_ts: messageTs,
   });
+
+  const skills = await database()
+    .select()
+    .from(schema.workSkills)
+    .innerJoin(schema.workSkillElements, eq(schema.workSkills.id, schema.workSkillElements.workSkillId))
+    // TODO: engineer、designerを判定して返すようにする
+    .where(eq(schema.workSkills.type, 'engineer'))
+    .orderBy(asc(schema.workSkills.level), asc(schema.workSkillElements.order))
+    .then((rows) => rows.reduce<Option[]>((previous, current) => {
+      const skill = previous.find((option) => option.id === current.work_skills.id);
+      if (skill) {
+        skill.elements.push({
+          id: current.work_skill_elements.id,
+          order: current.work_skill_elements.order,
+          name: current.work_skill_elements.name,
+        });
+        return previous;
+      }
+      previous.push({
+        id: current.work_skills.id,
+        name: current.work_skills.name,
+        level: current.work_skills.level,
+        elements: [
+          {
+            id: current.work_skill_elements.id,
+            order: current.work_skill_elements.order,
+            name: current.work_skill_elements.name,
+          },
+        ],
+      });
+      return previous;
+    }, []));
 
   await client.chat.postEphemeral({
     channel: channelId.value,
@@ -49,14 +94,23 @@ export const postQuestion = ResultAsync.fromThrowable(async (
             type: 'plain_text',
             text: 'スキルを選択する',
           },
-          options: [
-            {
+          option_groups: skills.map((skill) => ({
+            label: {
+              type: 'plain_text',
+              text: skill.name,
+            },
+            options: skill.elements.map((element) => ({
+              value: element.id,
               text: {
                 type: 'plain_text',
-                text: 'スキル1-1 (ミスが起きた際、自力ですぐに修正できる)',
+                text: `スキル${skill.level}-${element.order}`,
               },
-            },
-          ],
+              description: {
+                type: 'plain_text',
+                text: element.name,
+              },
+            })),
+          })),
         },
       },
       {
