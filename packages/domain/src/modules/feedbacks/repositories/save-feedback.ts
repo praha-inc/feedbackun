ba94a4f460f1b7f27cb2f08cf96344b735668ed5
Karibash <y.karibayashi@gmail.com>
2024-09-10 00:35:24 +0900

    bugfix: スキル未指定時にエラーが発生する不具合を修正する

--- packages/domain/src/modules/feedbacks/repositories/save-feedback.ts
+++ packages/domain/src/modules/feedbacks/repositories/save-feedback.ts
@@ -1,5 +1,6 @@
 import { CustomError } from '@feedbackun/package-custom-error';
 import { database, schema } from '@feedbackun/package-database';
+import { doAsync } from '@feedbackun/package-neverthrow';
 import { ok, ResultAsync } from 'neverthrow';
 
 import { SlackMessageId } from '../../slack-messages';
@@ -23,41 +24,66 @@ export type SaveFeedback = (
   input: SaveFeedbackInput,
 ) => ResultAsync<Feedback, SaveFeedbackError>;
 
-export const saveFeedback: SaveFeedback = (input) => {
-  const result = ResultAsync.fromPromise(
-    database().batch([
-      database()
-        .insert(schema.feedbacks)
-        .values({
-          id: input.id.value,
-          sendSlackUserId: input.sendSlackUserId.value,
-          receiveSlackUserId: input.receiveSlackUserId.value,
-          slackMessageId: input.slackMessageId.value,
-          content: input.content,
-          createdAt: input.createdAt,
-        })
-        .returning(),
-      database()
-        .insert(schema.feedbackWorkSkills)
-        .values(
-          input.workSkillElementIds.map((workSkillElementId) => ({
-            feedbackId: input.id.value,
-            workSkillElementId: workSkillElementId.value,
-          })),
-        )
-        .returning(),
-    ]),
-    (error) => new SaveFeedbackUnexpectedError({ cause: error }),
-  );
+const insertFeedbackWithSkills = (feedback: Feedback) => ResultAsync.fromPromise(
+  database().batch([
+    database()
+      .insert(schema.feedbacks)
+      .values({
+        id: feedback.id.value,
+        sendSlackUserId: feedback.sendSlackUserId.value,
+        receiveSlackUserId: feedback.receiveSlackUserId.value,
+        slackMessageId: feedback.slackMessageId.value,
+        content: feedback.content,
+        createdAt: feedback.createdAt,
+      })
+      .returning(),
+    database()
+      .insert(schema.feedbackWorkSkills)
+      .values(
+        feedback.workSkillElementIds.map((workSkillElementId) => ({
+          feedbackId: feedback.id.value,
+          workSkillElementId: workSkillElementId.value,
+        })),
+      )
+      .returning(),
+  ]),
+  (error) => new SaveFeedbackUnexpectedError({ cause: error }),
+);
 
-  return result
+const insertFeedbackWithoutSkills = (feedback: Feedback) => ResultAsync.fromPromise(
+  database().batch([
+    database()
+      .insert(schema.feedbacks)
+      .values({
+        id: feedback.id.value,
+        sendSlackUserId: feedback.sendSlackUserId.value,
+        receiveSlackUserId: feedback.receiveSlackUserId.value,
+        slackMessageId: feedback.slackMessageId.value,
+        content: feedback.content,
+        createdAt: feedback.createdAt,
+      })
+      .returning(),
+  ]),
+  (error) => new SaveFeedbackUnexpectedError({ cause: error }),
+);
+
+export const saveFeedback: SaveFeedback = (input) => {
+  return doAsync
+    .andThen(() => {
+      if (input.workSkillElementIds.length <= 0) {
+        return insertFeedbackWithoutSkills(input);
+      }
+      return insertFeedbackWithSkills(input);
+    })
     .andThen(([[feedback], feedbackWorkSkills]) => {
       return ok(new Feedback({
         id: FeedbackId.create(feedback!.id)._unsafeUnwrap(),
         sendSlackUserId: SlackUserId.create(feedback!.sendSlackUserId)._unsafeUnwrap(),
         receiveSlackUserId: SlackUserId.create(feedback!.receiveSlackUserId)._unsafeUnwrap(),
         slackMessageId: SlackMessageId.create(feedback!.slackMessageId)._unsafeUnwrap(),
-        workSkillElementIds: feedbackWorkSkills.map((row) => WorkSkillElementId.create(row.workSkillElementId)._unsafeUnwrap()),
+        workSkillElementIds: feedbackWorkSkills?.map((row) => {
+          return WorkSkillElementId.create(row.workSkillElementId)._unsafeUnwrap();
+        }) ?? [],
         content: feedback!.content,
         createdAt: feedback!.createdAt,
       }));
