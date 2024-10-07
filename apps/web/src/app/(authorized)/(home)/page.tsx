e98155ff3498c05184472e88888c307dd6141a67
Karibash <y.karibayashi@gmail.com>
2024-10-07 14:05:58 +0900

    feature: ヘッダー及びサイドバーをLayoutページに変更する

--- apps/web/src/app/(authorized)/(home)/page.tsx
+++ apps/web/src/app/(authorized)/(home)/page.tsx
@@ -1,9 +1,3 @@
-import { Building2Icon, MessageSquareHeartIcon, UsersIcon } from 'lucide-react';
-import Link from 'next/link';
-
-import * as styles from './page.css';
-import { Button, ButtonIcon } from '../../../components/elements/button';
-
 import type { FC } from 'react';
 
 export type HomePageProps = {
@@ -12,40 +6,7 @@ export type HomePageProps = {
 
 const HomePage: FC<HomePageProps> = () => {
   return (
-    <div className={styles.wrapper}>
-      <header className={styles.header}>
-        <h1 className={styles.title}>Feedbackun</h1>
-      </header>
-      <aside className={styles.aside}>
-        <nav className={styles.nav}>
-          <Button variant="ghost" size="medium" borderless asChild>
-            <Link href="/">
-              <ButtonIcon>
-                <Building2Icon />
-              </ButtonIcon>
-              Slack Teams
-            </Link>
-          </Button>
-          <Button variant="ghost" size="medium" borderless asChild>
-            <Link href="/">
-              <ButtonIcon>
-                <UsersIcon />
-              </ButtonIcon>
-              Users
-            </Link>
-          </Button>
-          <Button variant="ghost" size="medium" borderless asChild>
-            <Link href="/">
-              <ButtonIcon>
-                <MessageSquareHeartIcon />
-              </ButtonIcon>
-              Feedbacks
-            </Link>
-          </Button>
-        </nav>
-      </aside>
-      <main className={styles.main} />
-    </div>
+    <div />
   );
 };
 
---
+++ apps/web/src/app/(authorized)/layout.tsx
@@ -0,0 +1,56 @@
+import { Building2Icon, MessageSquareHeartIcon, UsersIcon } from 'lucide-react';
+import Link from 'next/link';
+
+import * as styles from './layout.css';
+import { Button, ButtonIcon } from '../../components/elements/button';
+
+import type { FC, ReactNode } from 'react';
+
+export type AuthorizedLayoutProps = {
+  children: ReactNode;
+};
+
+const AuthorizedLayout: FC<AuthorizedLayoutProps> = ({
+  children,
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Feedbackun</h1>
+      </header>
+      <aside className={styles.aside}>
+        <nav className={styles.nav}>
+          <Button variant="ghost" size="medium" borderless asChild>
+            <Link href="/">
+              <ButtonIcon>
+                <Building2Icon />
+              </ButtonIcon>
+              Slack Teams
+            </Link>
+          </Button>
+          <Button variant="ghost" size="medium" borderless asChild>
+            <Link href="/">
+              <ButtonIcon>
+                <UsersIcon />
+              </ButtonIcon>
+              Users
+            </Link>
+          </Button>
+          <Button variant="ghost" size="medium" borderless asChild>
+            <Link href="/">
+              <ButtonIcon>
+                <MessageSquareHeartIcon />
+              </ButtonIcon>
+              Feedbacks
+            </Link>
+          </Button>
+        </nav>
+      </aside>
+      <main className={styles.main}>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default AuthorizedLayout;
