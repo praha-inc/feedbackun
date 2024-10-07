ccc7da1d0508846aa62fb36f8049c24fa99b9522
Karibash <y.karibayashi@gmail.com>
2024-10-07 13:57:54 +0900

    feature: ヘッダーを追加する

--- apps/web/src/app/(home)/page.css.ts
+++ apps/web/src/app/(home)/page.css.ts
@@ -2,23 +2,51 @@ import { style } from '@vanilla-extract/css';
 
 import { theme } from '../../themes';
 
-export const aside = style({
-  display: 'flex',
-  flexDirection: 'column',
-  padding: '16px',
-  maxWidth: '16rem',
+export const wrapper = style({
+  display: 'grid',
+  gridTemplateAreas: `
+    'header header'
+    'aside main'
+  `,
+  gridTemplateRows: 'auto 1fr',
+  gridTemplateColumns: 'auto 1fr',
+  width: '100dvw',
   height: '100dvh',
+});
+
+export const header = style({
+  gridArea: 'header',
+  display: 'flex',
   backgroundColor: theme.color.token.semantic.backgroundInset,
-  borderRight: 'solid 1px',
+  borderBottom: 'solid 1px',
   borderColor: theme.color.token.semantic.border,
+  padding: '16px',
 });
 
 export const title = style({
   textAlign: 'center',
+  margin: 0,
+  fontSize: '1.75rem',
+});
+
+export const aside = style({
+  gridArea: 'aside',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '16px',
+  minWidth: '16rem',
+  backgroundColor: theme.color.token.semantic.background,
+  borderRight: 'solid 1px',
+  borderColor: theme.color.token.semantic.border,
 });
 
 export const nav = style({
   display: 'flex',
   flexDirection: 'column',
   gap: '8px',
+});
+
+export const main = style({
+  gridArea: 'main',
+  backgroundColor: theme.color.token.semantic.backgroundInset,
 });
--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -12,37 +12,40 @@ export type HomePageProps = {
 
 const HomePage: FC<HomePageProps> = () => {
   return (
-    <aside className={styles.aside}>
-      <h1 className={styles.title}>
-        Feedbackun
-      </h1>
-      <nav className={styles.nav}>
-        <Button variant="ghost" size="medium" borderless asChild>
-          <Link href="/">
-            <ButtonIcon>
-              <Building2Icon />
-            </ButtonIcon>
-            Slack Teams
-          </Link>
-        </Button>
-        <Button variant="ghost" size="medium" borderless asChild>
-          <Link href="/">
-            <ButtonIcon>
-              <UsersIcon />
-            </ButtonIcon>
-            Users
-          </Link>
-        </Button>
-        <Button variant="ghost" size="medium" borderless asChild>
-          <Link href="/">
-            <ButtonIcon>
-              <MessageSquareHeartIcon />
-            </ButtonIcon>
-            Feedbacks
-          </Link>
-        </Button>
-      </nav>
-    </aside>
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Feedbackun</h1>
+      </header>
+      <aside className={styles.aside}>
+        <nav className={styles.nav}>
+          <Button variant="ghost" size="medium" borderless asChild>
+            <Link href="/">
+              <ButtonIcon>
+                <Building2Icon />
+              </ButtonIcon>
+              Slack Teams
+            </Link>
+          </Button>
+          <Button variant="ghost" size="medium" borderless asChild>
+            <Link href="/">
+              <ButtonIcon>
+                <UsersIcon />
+              </ButtonIcon>
+              Users
+            </Link>
+          </Button>
+          <Button variant="ghost" size="medium" borderless asChild>
+            <Link href="/">
+              <ButtonIcon>
+                <MessageSquareHeartIcon />
+              </ButtonIcon>
+              Feedbacks
+            </Link>
+          </Button>
+        </nav>
+      </aside>
+      <main className={styles.main} />
+    </div>
   );
 };
 
