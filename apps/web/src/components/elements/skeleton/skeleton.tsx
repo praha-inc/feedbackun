210c2b28ee9092a525608a7c07fe8df6c78de811
Karibash <y.karibayashi@gmail.com>
2024-11-07 08:34:36 +0900

    refactor: Skeletonにrefを渡せるようにする

--- apps/web/src/components/elements/skeleton/skeleton.tsx
+++ apps/web/src/components/elements/skeleton/skeleton.tsx
@@ -1,28 +1,37 @@
 import { clsx } from 'clsx';
+import { forwardRef } from 'react';
 
 import * as styles from './skeleton.css';
 
 import type { RecipeVariants } from '@vanilla-extract/recipes';
-import type { FC } from 'react';
+import type { ComponentPropsWithoutRef, ForwardRefRenderFunction } from 'react';
 
 type SkeletonVariants = NonNullable<RecipeVariants<typeof styles.wrapper>>;
 
 export type SkeletonRadius = SkeletonVariants['radius'];
 
-export type SkeletonProps = {
+export type SkeletonProps = ComponentPropsWithoutRef<'div'> & {
   className?: string | undefined;
   width?: string | undefined;
   height?: string | undefined;
   radius?: SkeletonRadius | undefined;
 };
 
-export const Skeleton: FC<SkeletonProps> = ({
+const SkeletonRender: ForwardRefRenderFunction<HTMLDivElement, SkeletonProps> = ({
   className,
   width = '1em',
   height = '1em',
   radius = 'normal',
-}) => {
+  ...props
+}, ref) => {
   return (
-    <div className={clsx(styles.wrapper({ radius }), className)} style={{ width, height }} />
+    <div
+      {...props}
+      ref={ref}
+      className={clsx(styles.wrapper({ radius }), className)}
+      style={{ width, height }}
+    />
   );
 };
+
+export const Skeleton = forwardRef(SkeletonRender);
