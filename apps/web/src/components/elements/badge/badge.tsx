280cc8bb75ef0472ded79ac211d5c6142f64f691
Karibash <y.karibayashi@gmail.com>
2024-11-07 08:59:47 +0900

    refactor: Badgeにrefを渡せるようにする

--- apps/web/src/components/elements/badge/badge.tsx
+++ apps/web/src/components/elements/badge/badge.tsx
@@ -1,28 +1,32 @@
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
+import { forwardRef } from 'react';
 
 import * as styles from './badge.css';
 
-import type { ComponentPropsWithoutRef, FC } from 'react';
+import type { ComponentPropsWithoutRef, ElementRef, ForwardRefRenderFunction } from 'react';
 
 export type BadgeProps = ComponentPropsWithoutRef<'div'> & {
   asChild?: boolean | undefined;
 };
 
-export const Badge: FC<BadgeProps> = ({
+const BadgeRender: ForwardRefRenderFunction<ElementRef<'div'>, BadgeProps> = ({
   className,
   children,
   asChild,
   ...props
-}) => {
+}, ref) => {
   const Wrapper = asChild ? Slot : 'div';
 
   return (
     <Wrapper
       {...props}
+      ref={ref}
       className={clsx(styles.wrapper, className)}
     >
       {children}
     </Wrapper>
   );
 };
+
+export const Badge = forwardRef(BadgeRender);
