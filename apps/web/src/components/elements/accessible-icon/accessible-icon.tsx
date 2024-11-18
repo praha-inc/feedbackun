12bc1a526a7e509e562de31035d624e99545a693
Karibash <y.karibayashi@gmail.com>
2024-11-18 11:28:21 +0900

    feature: アイコンに対して意味のあるラベルを付けるコンポーネントを追加する

---
+++ apps/web/src/components/elements/accessible-icon/accessible-icon.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import * as AccessibleIconPrimitive from '@radix-ui/react-accessible-icon';
+
+import type { ComponentPropsWithoutRef } from 'react';
+
+export type AccessibleIconProps = ComponentPropsWithoutRef<typeof AccessibleIconPrimitive.Root>;
+
+export const AccessibleIcon = AccessibleIconPrimitive.Root;
